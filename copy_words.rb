d30d64ffdbbe6e9e52836fe51089537f81122437
allen8807 <zhaobl880704@gmail.com>
2019-12-07 17:17:07 +0800

    add files“

---
+++ copy_words.rb
@@ -0,0 +1,23 @@
+require 'fileutils'
+dirname = gets
+dirname.strip!
+FileUtils.mkdir_p(dirname)
+outfile = File.open(dirname + "/" + dirname + "_failed_words.txt", "a+")
+File.open("list.txt", "r") do |file|
+  file.each_line { |line|
+    line.strip!
+    name = line + ".mp3"
+    fullName = "voice/" + line[0, 1] + "/" + name
+    puts fullName
+    if (FileTest.exist?(fullName))
+      FileUtils.cp_r(fullName, dirname + "/" + name)
+      puts name + " was copied!"
+    else
+      puts name + " is not existed!"
+      outfile.puts line
+    end
+  }
+  file.close
+  outfile.close
+end
+
